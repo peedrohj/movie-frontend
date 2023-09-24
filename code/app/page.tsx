92aef5476ec15a10252c66fa58cb8dc6c70942d6
Pedro Farias <phjfg@cesar.org.br>
2023-09-24 19:52:11 +0000

    feat: add theme

--- code/app/page.tsx
+++ code/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen md:flex-row min-w-full flex-col-reverse items-center justify-between">
       <div
-        className="bg-black min-h-screen lg:w-3/5 md:w-2/5 w-full"
+        className="bg-background min-h-screen lg:w-3/5 md:w-2/5 w-full"
         style={{
           backgroundImage: `url(${bg.src})`,
           backgroundSize: "cover",
@@ -32,7 +32,7 @@ export default function Home() {
           backgroundRepeat: "no-repeat",
         }}
       ></div>
-      <div className="bg-black flex min-h-screen lg:w-2/5  md:w-3/5 md:px-24 w-full flex-row items-center justify-center ">
+      <div className="bg-background flex min-h-screen lg:w-2/5  md:w-3/5 md:px-24 w-full flex-row items-center justify-center ">
         <Card>
           <CardHeader>
             <CardTitle>Welcome to our plataform!</CardTitle>
