2efece7f55d3c479a3608c851544ef222b702b34
Pedro Farias <phjfg@cesar.org.br>
2023-10-23 18:35:54 +0000

    feat: show date in movie card

--- code/components/feature/movie/movie-card.tsx
+++ code/components/feature/movie/movie-card.tsx
@@ -11,38 +11,37 @@ import Link from "next/link";
 type MoviePropos = {
   params: {
     id: string;
+    size: string;
+    created_at: string;
   };
 };
 
-import VideoThumbnail from 'react-video-thumbnail';
 
-function MovieCard({ params: { id } }: MoviePropos) {
+function MovieCard({ params: { id, size, created_at } }: MoviePropos) {
   return (
     <Link key={id} href={`/movie/${id}`}>
-      <Card className="min-w-[350px]">
-        <CardHeader>
-          {/* <VideoThumbnail
-          videoUrl={`http://localhost:5001/core/movie/${id}/video/download`}
-          thumbnailHandler={(thumbnail) => console.log(thumbnail)}
-          width={450}
-          height={300}
-          snapshotAtTime={5}
-          /> */}
+      <Card className="min-w-[300px]">
+        <CardContent className="p-0">
           <img
             src="https://media.istockphoto.com/id/529249803/pt/foto/fundo-de-folhas-de-cor-verde-brilhante.webp?s=2048x2048&w=is&k=20&c=vqI7AUw-FFoqMW631yxdNwNNLDdnOLCgT_wWbQxc8RY="
             alt="Card Image"
             className="w-full"
           />
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
         </CardContent>
         <CardHeader>
           <CardTitle>Nome do filme</CardTitle>
-          <CardDescription>00:00</CardDescription>
+          <CardDescription>Duração: {size}</CardDescription>
+          <CardDescription>
+            Adicionado em{" "}
+            {new Date(created_at).toLocaleDateString("pt-br", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </CardDescription>
         </CardHeader>
-    </Card>
-      </Link>
+      </Card>
+    </Link>
   );
 }
 
--- code/components/feature/movie/movie-list.tsx
+++ code/components/feature/movie/movie-list.tsx
@@ -6,142 +6,15 @@ import MovieCardSkeleton from "./movie-card-skeleton";
 import { Separator } from "@radix-ui/react-separator";
 
 function MovieList() {
-  // const { data, error, isLoading, refetch } = useGetAllMoviesQuery({});
-  const data = [
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-    [
-      {
-        id: "f4f0e71c-e482-4a77-b305-2336d119aded",
-        created_at: "2023-09-24T01:18:53.972902",
-        updated_at: null,
-        deleted_at: null,
-        archived_at: null,
-        name: "003.mp4",
-        description: "None",
-        file_id: "1b0b0b5e-a1af-43ce-895e-7ccb5b397aeb",
-        size: "292.2491521835327",
-        content_type: "video/mp4",
-      },
-    ],
-  ];
+  const { data, error, isLoading, refetch } = useGetAllMoviesQuery({});
+
   return (
     <div className="flex flex-row">
       <div className="p-5 md:basis-4/5 items-center grid grid-cols-1 md:grid-cols-4 gap-4 max-w-fit">
         {/* {isLoading && <MovieCardSkeleton />} */}
         {data &&
           data.map((movie: Movie) => (
-            <MovieCard key={movie.id} params={{ id: movie.id }} />
+            <MovieCard key={movie.id} params={{ id: movie.id, size: movie.size, created_at: movie.created_at }} />
           ))}
       </div>
       
